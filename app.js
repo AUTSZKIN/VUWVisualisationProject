05255eeb3cb3a30e53f86ee399a70202a74eb116
AUTSZKIN <ojautszkin@gmail.com>
2021-12-05 22:24:57 +1300

    add Procfile

--- app.js
+++ app.js
@@ -45,3 +45,23 @@ function drawLocalData() {
 //   console.log(dag_data);
 //   return d3dag.dagStratify()(dag_data)”
 // }
+
+var PORT = process.env.PORT || 5000;
+var express = require("express");
+var app = express();
+var http = require("http");
+var server = http.Server(app);
+
+app.use(express.static("client"));
+
+server.listen(PORT, function () {
+  console.log("Chat server running");
+});
+
+var io = require("socket.io")(server);
+
+io.on("connection", function (socket) {
+  socket.on("message", function (msg) {
+    io.emit("message", msg);
+  });
+});
