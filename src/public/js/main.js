97599b07a59018956bd902bf2a391b327c2d76a0
AUTSZKIN <ojautszkin@gmail.com>
2021-12-06 09:28:12 +1300

    Code clean up

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -50,11 +50,11 @@ function drawHeader() {
       </div>
       
       <div id="site-intro">
-        <a href="https://vuwvisual.herokuapp.com/"><h2 id="maintitle">I LOVE ABI</h2></a>
+        <a href="https://vuwvisual.herokuapp.com/"><h2 id="maintitle">Course Pathway Visualisation</h2></a>
         <a href="https://www.wgtn.ac.nz/ecs"><h3 id="schooltitle">School of <a id="schoolname"> Engineering and Computer Science</a></h3></a>
       </div>
       `
-  ); // Course Pathway Visualisation
+  );
   const mainheader = d3.select("body").append("div").attr("id", "mainheader");
   mainheader.append("h3").text("Course Filter");
 }
