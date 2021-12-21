1f8fd289969fa822138565bd876598f479db3f04
AUTSZKIN <ojautszkin@gmail.com>
2021-12-21 14:48:53 +1300

    rearranged files

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-// /**
-//  * node.js应用由哪几部分组成：
-//  * 1. 引入required模块：我们可以使用 require 指令来载入 Node.js 模块。
-//  * 2. 创建服务器：服务器可以监听客户端的请求。
-//  * 3. 接收请求与响应请求：服务器很容易创建，客户端可以使用浏览器或终端发送 HTTP 请求，服务器接收请求后返回响应数据。
-//  */
+/**
+ * node.js应用由哪几部分组成：
+ * 1. 引入required模块：我们可以使用 require 指令来载入 Node.js 模块。
+ * 2. 创建服务器：服务器可以监听客户端的请求。
+ * 3. 接收请求与响应请求：服务器很容易创建，客户端可以使用浏览器或终端发送 HTTP 请求，服务器接收请求后返回响应数据。
+ */
 
 // import * as express from "http://unpkg.com/express@4.17.1?module"; // "bare" import d3-dag remotely using unpkg
-// var express = require("express");
 
-import express from "express";
-import path from "path";
-import d3dag from "d3-dag";
-
-const __dirname = path.resolve();
+var express = require("express");
 var app = express();
-
-console.log("D3 dag is here:");
-console.log(d3dag);
 
 app.set("port", process.env.PORT || 5000);
 
 //Express:这里设置了index.html要读取的所有文档js起始点，所以路径是从这里开始的，注意了！
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/src"));
 
 // views is directory for all template files
 app.set("views", __dirname + "/src/views");
--- src/public/js/appentry.js
+++ src/public/js/appentry.js
@@ -3,12 +3,12 @@
 
 /***************************************/
 
-import * as d3dag from "/src/public/d3-dag/d3-dag-047.js";
+import * as d3dag from "/public/d3-dag/d3-dag-047.js";
 import { draw } from "./main.js";
 import sugiyama from "./sugiyamaDagDrawing.js";
 
 const courseJsonDataUrl =
-  "https://raw.githubusercontent.com/AUTSZKIN/VUWVisualisationProject/main/src/data/converted/ecs-sms-courses.json";
+  "https://raw.githubusercontent.com/AUTSZKIN/VUWVisualisationProject/main/src/dataAndParser/converted/ecs-sms-courses.json";
 
 drawLocalData();
 function drawLocalData() {
